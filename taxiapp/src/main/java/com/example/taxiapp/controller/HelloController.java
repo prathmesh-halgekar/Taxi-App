c387246d968eb3c0c6e6bb9c505be30df0c909d3
prathmesh-halgekar <prats_apr25@yahoo.com>
2017-10-06 19:07:23 +0200

    Registeration added

--- taxiapp/src/main/java/com/example/taxiapp/controller/HelloController.java
+++ taxiapp/src/main/java/com/example/taxiapp/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.taxiapp.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.taxiapp.model.Customer;
 
 @RestController
 public class HelloController {
@@ -11,5 +14,11 @@ public class HelloController {
     public String loginSuccess() {
 		
 	    return "Success123";        
+    }
+	
+	@RequestMapping(value="/register", method = RequestMethod.POST, produces="application/json")
+    public String createUser(@RequestBody Customer customer) {
+		
+	    return "Customer : "+customer.getFirstName()+" registered successfully.";        
     }
 }
